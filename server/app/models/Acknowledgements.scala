c303ba1e9a3c55b2bb974c98a0d283fcbe014639
ianknowles <ian@imknowles.co.uk>
2021-10-13 18:06:51 +0100

    dgg acknowledgements

--- server/app/models/Acknowledgements.scala
+++ server/app/models/Acknowledgements.scala
@@ -5,10 +5,24 @@ import controllers.routes.Assets
 object Acknowledgements {
 	val primary: List[Acknowledgement] = List(
 		Acknowledgement(
-			href = "https://www.ox.ac.uk/",
+			href = "https://www.sociology.ox.ac.uk/",
 			alt = "University of Oxford logo",
 			image = Assets.versioned("images/logos/oxweb-logo-rect.svg"),
 			imageCSSClass = "oxford",
+		),
+		Acknowledgement(
+			href = "https://www.qcri.org.qa/",
+			alt = "Hamad Bin Khalifa University logo",
+			image = Assets.versioned("images/logos/qcri28rgb29-01.svg"),
+			imageCSSClass = "qcri",
+		)
+	)
+	val support: List[Acknowledgement] = List(
+		Acknowledgement(
+			href = "https://www.data2x.org/",
+			alt = "data2x project logo",
+			image = Assets.versioned("images/logos/data2x-logo.png"),
+			imageCSSClass = "data2x",
 		),
 	)
 }
